ad011510647cc679f2926377397099b06a664f32
unknown <dubeyanuj12345@gmail.com>
2024-02-02 00:07:30 +0530

    request and response for url done

--- first-app.js
+++ first-app.js
@@ -1,9 +1,26 @@
 const http = require('http')
 
 const server = http.createServer((req,res) => {
+
     //console.log(req.url, req.method, req.headers)
-    res.write('Hello in the world of node.js')
-    res.end()
+    const url = req.url
+
+    if(url === '/home'){
+        res.write('Welcome home');
+        res.end();
+    }
+    else if( url === '/about' ){
+        res.write('Welcome to About Us page');
+        res.end();
+    }
+    else if(url === '/node'){
+        res.write('Welcome to my Node Js project');
+        res.end();
+    }
+    else{
+        res.write('Hello');
+        res.end();
+    }
 })
 
 server.listen(4000)
