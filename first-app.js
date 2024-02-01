79d6da69cbd46528fa3c2e745a0cc358d900c88c
unknown <dubeyanuj12345@gmail.com>
2024-02-01 22:45:34 +0530

    server created

--- first-app.js
+++ first-app.js
@@ -1,1 +1,9 @@
-console.log('Hello World');
+const http = require('http')
+
+const server = http.createServer((req,res) => {
+    //console.log(req.url, req.method, req.headers)
+    res.write('Hello in the world of node.js')
+    res.end()
+})
+
+server.listen(4000)
