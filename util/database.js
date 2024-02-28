6f34a4c2748532ac9407c05d422f66099478a634
unknown <dubeyanuj12345@gmail.com>
2024-02-28 16:51:01 +0530

    Starting with MYSQL

---
+++ util/database.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password:'1234'
+});
+
+
+module.exports = pool.prommise();
